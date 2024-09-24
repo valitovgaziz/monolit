543e710fd002e905cb565ac38a5fd6b62c2a6302
valitovgaziz <valitovgaziz@gmail.com>
2024-09-25 03:25:54 +0500

    added Header and SearcBar components

---
+++ front/src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+    <header class="header">
+      <!-- Логотип -->
+      <img src="logo.png" alt="Logo" />
+      
+      <!-- Выпадающее меню справа -->
+      <nav class="menu-right">
+        <ul>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Sign Up</a></li>
+          <li><a href="#">About Us</a></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #fff;
+  border-bottom: 1px solid #ccc;
+}
+
+.menu-right {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu-right li {
+  display: inline-block;
+  margin-right: 1rem;
+}
+
+.menu-right a {
+  color: #333;
+  text-decoration: none;
+}
+
+.menu-right a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ front/src/components/SearchBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="search-bar">
+      <input type="text" placeholder="Search..." v-model="query"/>
+      <button @click="handleSubmit">Search</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SearchBar',
+    data() {
+      return {
+        query: ''
+      };
+    },
+    methods: {
+      handleSubmit() {
+        // Код для обработки отправки поискового запроса
+        console.log('Поисковый запрос:', this.query);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .search-bar {
+    display: flex;
+    width: 100%;
+    max-width: 400px;
+    margin: 1rem auto;
+  }
+  
+  .search-bar input {
+    width: 100%;
+    height: 36px;
+    padding: 8px 12px;
+    outline: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  
+  .search-bar button {
+    height: 36px;
+    padding: 0 12px;
+    background-color: #ccc;
+    border: none;
+    cursor: pointer;
+    border-radius: 4px;
+  }
+  </style>
