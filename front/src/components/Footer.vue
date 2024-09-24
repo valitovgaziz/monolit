7503edba6f8148b979e4b41ef4daa24d93b36ee7
valitovgaziz <valitovgaziz@gmail.com>
2024-09-25 03:29:03 +0500

    Footer component for Main page added

---
+++ front/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+      <p>© 2023 Company Name Inc. All rights reserved.</p>
+      <p>Company Address: 123 Main Street, City, State ZIP</p>
+      <p>Phone Number: +1 (XXX) XXX-XXXX</p>
+      <p>Email: <EMAIL></p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer'
+  }
+  </script>
+  
+  <style scoped>
+  .footer {
+    text-align: center;
+    padding: 1rem;
+    background-color: #eee;
+    border-top: 1px solid #ccc;
+  }
+  </style>
