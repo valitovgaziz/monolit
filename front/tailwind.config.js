5a638468defe86c954fe2b74e73ff6c4cb69cb0c
valitovgaziz <valitovgaziz@gmail.com>
2024-09-29 05:44:31 +0500

    Add tailwind

---
+++ front/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif']
+    }
+  },
+  plugins: []
+}
