160847f65ce5a147e82100d74fbfcf41fa725d70
valitovgaziz <valitovgaziz@gmail.com>
2024-10-06 05:23:43 +0500

    Init one more front project

---
+++ front/src/components/AddTodoInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div
+      class="px-5 w-full bg-gray-100 rounded-lg outline-none text-gray-900 flex items-center justify-between"
+    >
+      <input
+        class="rounded-lg py-3.5 flex-1 bg-gray-100 outline-none pr-2.5"
+        type="text"
+        placeholder="What needs to be done?"
+      />
+    </div>
+  </template>
+  
+  <script setup></script>
